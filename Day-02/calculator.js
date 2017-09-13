9cc62698e16e9a3c5a5e9c0b91530db951868ffe
Magesh <tkmagesh77@gmail.com>
2017-09-13 10:00:53 +0530

    Task Manager - initial

---
+++ Day-02/calculator.js
@@ -0,0 +1,14 @@
+var calculator = {
+	add : function(operands){
+		return operands.n1 + operands.n2;
+	},
+	subtract : function(operands){
+		return operands.n1 - operands.n2;
+	},
+	multiply : function(operands){
+		return operands.n1 * operands.n2;
+	},
+	divide : function(operands){
+		return operands.n1 / operands.n2;
+	}
+};
